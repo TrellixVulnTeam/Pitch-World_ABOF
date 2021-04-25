06ce106527a10b31cc2cfc288c59835131dfe7a7
Marya <maryammuchai@gmail.com>
2021-04-25 09:58:42 +0300

    adding the comment sections and pitch

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+
 from config import config_options
 
 # from config import Config
 app = Flask(__name__)
 
+#Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
+
 #Initializing flask extension
-bootstrap = Bootstrap()
-db = SQLAlchemy()
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 #
 def create_app(config_name):
--- config.py
+++ config.py
@@ -4,3 +4,14 @@ class Config:
     debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://maryam:maryam@localhost/pitch'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG =True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
