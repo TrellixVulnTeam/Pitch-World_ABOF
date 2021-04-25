73577a593d7df380a1feb5e5789596747f56b61d
Marya <maryammuchai@gmail.com>
2021-04-25 21:39:08 +0300

    adding the virtual detail

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+from flask import Flask 
 from config import config_options
 
-# from config import Config
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-
-
+db = SQLAlchemy()
+bootstap = Bootstrap()
 
 def create_app(config_name):
+    app = Flask(__name__)
     app.config.from_object(config_options[config_name])
-  
+   
+    db.init_app(app)
+    bootstap.init_app(app)
+    
     return app
--- manage.py
+++ manage.py
@@ -4,11 +4,13 @@ from app.models import User
 
 #Creating app instance
 app = create_app('production')
+
 manager = Manager(app)
 manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User)
+    
 if __name__ == '__main__':
     manager.run()
