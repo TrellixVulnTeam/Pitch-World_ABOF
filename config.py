38301d071ca94794587a7f9d6d93916773f6bd99
Marya <maryammuchai@gmail.com>
2021-04-25 22:05:49 +0300

    resolving db 'NoneType' error

--- config.py
+++ config.py
@@ -2,15 +2,17 @@ import os
 
 
 class Config:
-    debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:maryam@localhost/pitch'
 
+    @staticmethod
+    def init_app(app):
+        pass
+
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:maryam@localhost/pitch'
     DEBUG =True
 
 config_options = {
