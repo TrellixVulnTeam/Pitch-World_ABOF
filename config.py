58c4b5f4c3a9fd20848033fec74a0dee426c00dc
Marya <maryammuchai@gmail.com>
2021-04-26 13:29:19 +0300

    linking the comment id to the post id

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:maryam@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:maryam @localhost/pitch'
 
     @staticmethod
     def init_app(app):
