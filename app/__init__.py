3f0976015c547263a54bface2a2ff18960802a0e
Marya <maryammuchai@gmail.com>
2021-04-27 12:05:47 +0300

    creating a file upload form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from config import config_options
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -7,9 +8,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 bootstap = Bootstrap()
 
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+
+    #configure UploadSet
+    configure_uploads(app,photos) 
    
     db.init_app(app)
     bootstap.init_app(app)
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryam:1234@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     @staticmethod
     def init_app(app):
