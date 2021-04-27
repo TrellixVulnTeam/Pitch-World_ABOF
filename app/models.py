31b31fafbfeb8813a93b303a1bd79622ff1803c7
Marya <maryammuchai@gmail.com>
2021-04-27 11:30:30 +0300

    Registering new users by creating the register form

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
-from . import db
+from . import db ,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,7 +17,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
