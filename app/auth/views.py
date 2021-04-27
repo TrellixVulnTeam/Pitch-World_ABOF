f9c6f12aeabb2ff5354bb9f9db512336253281eb
Marya <maryammuchai@gmail.com>
2021-04-27 11:40:05 +0300

    updating flask login to the navbar

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,38 @@
-from flask import render_template
+from flask import render_template, redirect,url_for , flash,request
 from . import auth
+from flask_login import login_user
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+@main.route('/')
+def index():
+    posts = Post.query.all()
+    product = Post.query.filter_by(category='product').all()
+    idea = Post.query.filter_by(category='idea').all()
+    business = Post.query.filter_by(category='Business').all()
+    return render_template('index.html', business=business, product=product, idea=idea, posts=posts)
+
+
+@main.route('/posts')
+@login_required
+def posts():
+    posts = Post.query.all()
+    likes = Upvote.query.all()
+    user = current_user
+    return render_template('pitch_display.html', posts=posts, likes=likes, user=user)
+
+
+@main.route('/new_post', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        post_obj = Post(post=post, title=title, category=category, user_id=user_id)
+        post_obj.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html', form=form)
