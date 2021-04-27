d7c3765bc4657f5c5b2323ee2516d64b20907d65
Marya <maryammuchai@gmail.com>
2021-04-27 11:26:22 +0300

    adding authenticated routes and flask login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,16 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
 class User(db.Model):
     __tablename__='users'
@@ -7,6 +19,39 @@ class User(db.Model):
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def set_password(self, password):
+        pass_hash = generate_password_hash(password)
+        self.password = pass_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+        
+        pass_secure  = db.Column(db.String(255))
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+
     def __repr__(self):
-        return f'User {self.user_id}'
+        return f'User {self.username}'
+
+
+
 
