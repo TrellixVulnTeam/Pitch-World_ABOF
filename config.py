83f46d44dac416a2ac106e5d2a6f11dc18647133
Marya <maryammuchai@gmail.com>
2021-05-01 18:19:50 +0300

    deploying the app

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://","ql://",1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class DevConfig(Config):
