fb3e3002df23c47b962e164c50a0c1722932b15e
Marya <maryammuchai@gmail.com>
2021-05-01 15:42:54 +0300

    setting the prodconfig set up

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://","ql://",1)
 
 
 class DevConfig(Config):
