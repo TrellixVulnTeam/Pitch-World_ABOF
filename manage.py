46db5012b69a32246a710ab7cdcfe6488240f7cc
Marya <maryammuchai@gmail.com>
2021-05-03 11:45:39 +0300

    changing the initialization code structure

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+
 #Creating app instance
 app = create_app('production')
 manager = Manager(app)
-manager.add_command('server',Server)
+
 migrate = Migrate(app,db)
+manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
 @manager.command
 def test():
