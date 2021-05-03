5a2490683c6cbe5040a05b1bb7ebac8eb53a774b
Marya <maryammuchai@gmail.com>
2021-05-03 15:51:17 +0300

    setting the .gitignore

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME='maryammuchai@gmail.com'
 export MAIL_PASSWORD='maryam'
 
-export SECRET_KEY='SECRET_KEY'
+export SECRET_KEY='31db5ee4-ac0c-11eb-b777-73a3c8d81bde'
 
 python3 manage.py server
